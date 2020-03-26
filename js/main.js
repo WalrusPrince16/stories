0c490615b921d0e87d109f66cc661f826e3aa3a6
christian Kilian <christiankilian@mac.local>
2020-03-26 19:32:20 -0400

    added menu UI

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
+// Header
+
 const header = document.getElementById('header');
 const mainContent = document.getElementById("main-content");
-
 let headerShow = mainContent.offsetTop;
 
 window.addEventListener("scroll", toggleHeader);
@@ -13,6 +14,8 @@ function toggleHeader() {
     }
 }
 
+// HEADER
+
 const story = document.getElementById('story');
 const fontPlus = document.getElementById('fontPlus');
 const fontMinus = document.getElementById('fontMinus');
@@ -24,7 +27,7 @@ fontPlus.addEventListener('click', function(){
     getStoryLength();
     updateProgress();
     fontSize++;
-    if (fontSize >= 5) {
+    if (fontSize >= 6) {
         fontSize = 2;
         story.style.fontSize = "2rem";
     } else {
@@ -46,22 +49,85 @@ fontMinus.addEventListener('click', function(){
     zoomLvl.innerText = `${fontSize}x`;
 });
 
+// PROGRESS METER
+
 const progress = document.getElementById('progress');
 
 window.addEventListener("scroll", updateProgress);
 
 function getStoryLength() {
-    console.log(story.clientHeight);
     return story.clientHeight;
 }
 
 function updateProgress() {
-    let percent = 
-    Math.floor((window.pageYOffset / getStoryLength()) * 100);
-    if (percent <= 0) {
-        progress.innerText = `0% read`;
+    if (progress.classList.contains("main__header-progress--done")) {
+        return;
     } else {
-        progress.innerText = `${percent}% read`;
+        let percent = 
+        Math.floor((window.pageYOffset / getStoryLength()) * 100);
+        if (percent <= 0) {
+            progress.innerText = `0% read`;
+        } else {
+            progress.innerText = `${percent}% read`;
+        }
     }
-    
 }
+
+// SLIDE OUT MENU
+
+const slideOutMenu = document.getElementById('main-menu');
+const storyText = document.querySelector(".main__story");
+const slideOutMenuContent = document.getElementById("main-menu-content")
+
+function slideOut() {
+    storyText.classList.toggle("main__story--menu-open");
+    slideOutMenu.classList.toggle("main__menu--menu-open");
+    slideOutMenuContent.classList.toggle("main__menu-content--menu-open");
+};
+
+slideOutMenu.addEventListener("click", slideOut);
+
+// FORM
+
+const menuOptions = document.getElementById('menu-form');
+const hyphens = document.getElementById('hyphens');
+const done = document.getElementById('done');
+const themes = document.getElementById('themes');
+const main = document .getElementById('main');
+
+let selectedOptions = {
+    hyphens: true,
+    done: false,
+    theme: "default"
+}
+
+function updateSettings(e) {
+    e.preventDefault();
+    selectedOptions.hyphens = hyphens.checked;
+    selectedOptions.done = done.checked;
+    selectedOptions.theme = themes.value;
+    updateUI();
+}
+
+function updateUI() {
+    if (selectedOptions.hyphens) {
+        storyText.classList.add("main__story--hyphens");
+    } else {
+        storyText.classList.remove("main__story--hyphens");
+    }
+    if (selectedOptions.done) {
+        progress.classList.add("main__header-progress--done");
+        progress.innerHTML = "Completed " + "<i class='fas fa-check'></i>";
+        window.removeEventListener("scroll", updateProgress);
+    } else {
+        progress.classList.remove("main__header-progress--done");
+        updateProgress();
+        window.addEventListener("scroll", updateProgress);
+    }
+
+    main.className = `main main--${selectedOptions.theme}`;
+}
+
+updateUI();
+
+menuOptions.addEventListener("submit", updateSettings);
